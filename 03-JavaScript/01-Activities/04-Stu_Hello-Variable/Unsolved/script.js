1cc1023135f9dbcc42cdfa97d64d363856282fcc
Magerick <112679606+Magerick@users.noreply.github.com>
2022-09-21 17:18:05 -0700

    Fixed folder structures (Unit 3 in Unit 2 folder).

---
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,12 @@
+// Write Your JavaScript Code Here
+var personName = "Dan Magerick Ponce";
+var pets = "0";
+var funFact = "I made a C++ algorithm for fun once.";
+
+personName = "Cobalt";
+pets = "1";
+funFact = "I'm alive.";
+
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pets.");
+console.log("Fun fact: "+ funFact);
---
+++ 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -0,0 +1,26 @@
+var one = 64; //myNumberInt
+var two = "656302"; //string
+var three = false; //ismyBooleanFalse
+var four = 64.55; //myNumberDecimal
+var five = "Howdy!"; //string
+var six; //undefined
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof one); 
+console.log(typeof two); 
+console.log(typeof three); 
+console.log(typeof four); 
+console.log(typeof five);
+console.log(typeof six);
+//console.log(typeof myNumberInt); logs numbers above.
+
+four = "Hello!";
+five = false;
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof four);
+console.log(typeof five);
+console.log(typeof six);
+//typeof four will log #4 as "Hello" string, typeof five will log a ismyBooleanFalse (false),
+//and typeof six will log a myNumberInt (23).
